931da9389da74e28ea23483fc771f6907b32fe86
danysainero <danysainero@gmail.com>
2019-05-20 19:19:10 +0200

    version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,428 @@
+const contacts = [{
+    "name": "Piedad",
+    "surname": "Dunfee",
+    "email": "piedad.dunfee@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Elsie",
+    "surname": "Calvery",
+    "email": "elsie.calvery@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Rodger",
+    "surname": "Pye",
+    "email": "rodger.pye@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Mohammad",
+    "surname": "Pye",
+    "email": "mohammad.pacheo@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Emmie",
+    "surname": "Derryberry",
+    "email": "emmie.derryberry@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Lino",
+    "surname": "Kottwitz",
+    "email": "lino.kottwitz@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Brittney",
+    "surname": "Ruddock",
+    "email": "brittney.ruddock@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "familia"
+  },
+  {
+    "name": "Jaimie",
+    "surname": "Human",
+    "email": "jaimie.human@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Darleen",
+    "surname": "Kesten",
+    "email": "darleen.kesten@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Georgie",
+    "surname": "Hannold",
+    "email": "georgie.hannold@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "familia"
+  },
+  {
+    "name": "Doreatha",
+    "surname": "Wheeler",
+    "email": "doreatha.wheeler@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Christie",
+    "surname": "Solum",
+    "email": "christie.solum@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Carlita",
+    "surname": "Whitmire",
+    "email": "carlita.whitmire@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "familia"
+  },
+  {
+    "name": "Luciana",
+    "surname": "Jobe",
+    "email": "luciana.jobe@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Sade",
+    "surname": "Selig",
+    "email": "sade.selig@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Devon",
+    "surname": "Goodridge",
+    "email": "devon.goodridge@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Bruna",
+    "surname": "Starkes",
+    "email": "bruna.starkes@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Glynis",
+    "surname": "Hudnall",
+    "email": "glynis.hudnall@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Anissa",
+    "surname": "Leggett",
+    "email": "anissa.leggett@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "familia"
+  },
+  {
+    "name": "Moshe",
+    "surname": "Moxley",
+    "email": "moshe.moxley@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Shannon",
+    "surname": "Olivieri",
+    "email": "shannon.olivieri@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Janel",
+    "surname": "Vandever",
+    "email": "janel.vandever@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "favoritos"
+  },
+  {
+    "name": "Tashia",
+    "surname": "Fontes",
+    "email": "tashia.fontes@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Merrilee",
+    "surname": "Lynde",
+    "email": "merrilee.lynde@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Coral",
+    "surname": "Eastin",
+    "email": "coral.eastin@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Shandi",
+    "surname": "Hunsaker",
+    "email": "shandi.hunsaker@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  },
+  {
+    "name": "Wilburn",
+    "surname": "Meyer",
+    "email": "wilburn.meyer@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Quintin",
+    "surname": "Schrom",
+    "email": "quintin.schrom@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Claribel",
+    "surname": "Sipes",
+    "email": "claribel.sipes@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "trabajo"
+  },
+  {
+    "name": "Nevada",
+    "surname": "Chappelle",
+    "email": "nevada.chappelle@domain.com",
+    "country": "españa",
+    "phone": "607 123 456",
+    "group": "amigos"
+  }
+]
+
+
+$(document).ready(function () {
+
+  var contactosAll = contacts;
+  var favourites = ContactsFilter(contacts, "group", "favoritos");
+  var friends = ContactsFilter(contacts, "group", "amigos");
+  var family = ContactsFilter(contacts, "group", "familia");
+  var work = ContactsFilter(contacts, "group", "trabajo");
+  var btnAll = $("#all");
+  var btnFav = $("#fav");
+  var btnFriends = $("#friends");
+  var btnFamily = $("#family");
+  var btnWork = $("#work");
+  var ContactsList = $(".contacts__list");
+  var btnContactos = $(".btn--contactos");
+
+
+
+  //FUNCION PARA TODOS LOS CONTACTOS
+  btnAll.click(function () {
+    ContactsList.empty();
+
+    for (let i = 0; i < contactosAll.length; i++) {
+      var name = contactosAll[i].name;
+      var surname = contactosAll[i].surname;
+      var li = document.createElement("li");
+      li.setAttribute("class", "contact");
+      var button = document.createElement("button");
+      button.setAttribute("class", "btn btn--contactos");
+      button.setAttribute("type", "button");
+      button.innerText = `${name} ${surname}`;
+      li.prepend(button);
+      ContactsList.prepend(li);
+    }
+
+  });
+
+  //Añade onclick a contactos para seleccionarlos
+  $(document).on("click", ".btn--contactos", function (event) {
+    var fondo = random_bg_color();
+    var titulo = $(".details__title__container");
+    titulo.empty();
+    var info = $(".details__info__container");
+    info.empty();
+    var btn = $(".details__btn__container");
+    btn.empty();
+
+    var contactSelected = event.target.innerText;
+    var contactSelectedSeparated = contactSelected.split(" ", 2);
+    var miContacto = ContactsFilter(contacts, "name", `${contactSelectedSeparated[0]}`);
+    var firstWord = miContacto[0].name;
+    var primeraLetra = firstWord.substr(0, 1)
+    var group = miContacto[0].group;
+    var email = miContacto[0].email;
+    var phone = miContacto[0].phone;
+
+    var button = document.createElement("button");
+    button.innerHTML = 'Editar';
+    button.setAttribute("class", "btn show-contact");
+    btn.prepend(button);
+
+    var pe4 = document.createElement("p");
+    pe4.setAttribute("class", "phone");
+    pe4.innerText = phone;
+    info.prepend(pe4);
+
+    var pe3 = document.createElement("p");
+    pe3.setAttribute("class", "email");
+    pe3.innerText = email;
+    info.prepend(pe3);
+
+    var pe2 = document.createElement("p");
+    pe2.innerText = group;
+    titulo.prepend(pe2);
+
+    var pe = document.createElement("p");
+    pe.innerText = contactSelected;
+    var span = document.createElement("span");
+    span.innerText = primeraLetra;
+    span.style.backgroundColor = fondo;
+    span.setAttribute("class", "inicial");
+    titulo.prepend(pe);
+    titulo.prepend(span);
+
+  });
+
+
+  //FUNCION PARA TODOS LOS FAVS
+  btnFav.click(function () {
+    ContactsList.empty();
+    for (let i = 0; i < favourites.length; i++) {
+      var name = favourites[i].name;
+      var surname = favourites[i].surname;
+      var li = document.createElement("li");
+      li.setAttribute("class", "contact");
+      var button = document.createElement("button");
+      button.setAttribute("class", "btn btn--contactos");
+      button.setAttribute("type", "button");
+      button.innerText = `${name} ${surname}`;
+      li.prepend(button);
+      ContactsList.prepend(li);
+    }
+
+  });
+
+  //FUNCION PARA TODOS LOS AMIGUIS
+  btnFriends.click(function () {
+    ContactsList.empty();
+    for (let i = 0; i < friends.length; i++) {
+      var name = friends[i].name;
+      var surname = friends[i].surname;
+      var li = document.createElement("li");
+      li.setAttribute("class", "contact");
+      var button = document.createElement("button");
+      button.setAttribute("class", "btn btn--contactos");
+      button.setAttribute("type", "button");
+      button.innerText = `${name} ${surname}`;
+      li.prepend(button);
+      ContactsList.prepend(li);
+    }
+  });
+
+  //FUNCION PARA TODOS LA FAMILY
+  btnFamily.click(function () {
+
+    ContactsList.empty();
+
+    for (let i = 0; i < family.length; i++) {
+      var name = family[i].name;
+      var surname = family[i].surname;
+      var li = document.createElement("li");
+      li.setAttribute("class", "contact");
+      var button = document.createElement("button");
+      button.setAttribute("class", "btn btn--contactos");
+      button.setAttribute("type", "button");
+      button.innerText = `${name} ${surname}`;
+      li.prepend(button);
+      ContactsList.prepend(li);
+    }
+  });
+
+  //FUNCION PARA TODOS LOS WORKERS
+  btnWork.click(function () {
+    ContactsList.empty();
+
+    for (let i = 0; i < work.length; i++) {
+      var name = work[i].name;
+      var surname = work[i].surname;
+      var li = document.createElement("li");
+      li.setAttribute("class", "contact");
+
+      var button = document.createElement("button");
+      button.setAttribute("class", "btn btn--contactos");
+      button.setAttribute("type", "button");
+
+
+
+      button.innerText = `${name} ${surname}`;
+      li.prepend(button);
+      ContactsList.prepend(li);
+    }
+
+  });
+
+  //FUNCION FILTRA CONTACTOS
+  function ContactsFilter(array, key, value) {
+    return array.filter(function (e) {
+      return e[key] == value;
+    });
+  };
+
+  //MUESTRA LAS OPCIONES EDITAR
+  $(document).on("click", ".show-contact", function () {
+    $('.edit').toggleClass("show");
+  });
+
+  //setea un background diferente cada vez
+  function random_bg_color() {
+    var colores = ['#d0021b', '#02b4d0', '#5502d0', '#024bd0', '#c702d0', '#02d0ab', '#47d002'];
+    var numero_colores = colores.length;
+    var numero = Math.floor(Math.random() * numero_colores);
+    var fondo = (colores[numero]);
+    return fondo;
+  }
+
+});
